134170ba29fd3857f087d97f828806b8ec04032f
unknown <pachecomacias121@gmail.com>
2022-01-13 21:37:28 -0500

    se agrega mi proyecto

---
+++ Holamundo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/*
+Autor: Pacheco Angel
+Fecha: 09/01/2022
+*/
+namespace Pacheco
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa el numero a buscar de la lista");
+            int numeroBuscar=int.Parse(Console.ReadLine());
+            int[] listaNumeros = new int[7];
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("ingresa el elemento "+(i+1)+" de la lista");
+                int numero = int.Parse(Console.ReadLine());
+                listaNumeros[i] = numero;
+            }
+            int pos = 0;
+            bool bandera = false;
+            while (pos < 7 && bandera == false) {
+                if (numeroBuscar == listaNumeros[pos]) {
+                    bandera = true;
+                    break;
+                }
+                pos++;
+            }
+            if (bandera == true)
+            {
+                Console.WriteLine("Felicidades se encontro el numero " + numeroBuscar + " en la posicion " + (pos+1));
+            }
+            else {
+                Console.WriteLine("Lo siento no se encontro el numero "+numeroBuscar);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Holamundo/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("pacheco")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pacheco")]
+[assembly: AssemblyCopyright("1")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c2ff5a8-6b6f-4ade-8f0f-c571c50ede04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
